64fe9f94fe3c14bdf47b90fb0d98c132715ad3f5
Agentp <2569787219@qq.com>
2023-04-03 16:19:00 +0800

    create

---
+++ src/main/java/com/agentp/cepp/CeppApplication.java
@@ -0,0 +1,13 @@
+package com.agentp.cepp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agentp/cepp/CeppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agentp.cepp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
