48e2f5b53ffc1f5824619c6aa2099798a4476d44
Agentp <2569787219@qq.com>
2023-04-06 19:47:09 +0800

    optimize controllerCode

--- src/main/java/com/agentp/cepp/controller/BaseController.java
+++ src/main/java/com/agentp/cepp/controller/BaseController.java
@@ -1,7 +1,11 @@
 package com.agentp.cepp.controller;
 
+import com.agentp.cepp.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
+    @Autowired
+    UserInfoService userInfoService;
 }
