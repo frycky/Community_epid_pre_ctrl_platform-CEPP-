1c0dfdae9ce6e5911f770fa5c75c918640755943
Agentp <2569787219@qq.com>
2023-04-07 17:54:45 +0800

    jwt completed

--- src/main/java/com/agentp/cepp/controller/UserInfoController.java
+++ src/main/java/com/agentp/cepp/controller/UserInfoController.java
@@ -1,17 +1,20 @@
 package com.agentp.cepp.controller;
 
 
+import com.agentp.cepp.common.Result;
+import com.agentp.cepp.dto.PageInfo;
+import com.agentp.cepp.dto.UserLoginDto;
 import com.agentp.cepp.entity.UserInfo;
-import com.agentp.cepp.service.UserInfoService;
+import com.agentp.cepp.exception.CustomException;
+import com.agentp.cepp.utils.JwtUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -27,11 +30,79 @@ import java.util.List;
 @RequestMapping("/user-info")
 public class UserInfoController extends BaseController {
 
-    @Autowired
-    private UserInfoService userInfoService;
+
     @ApiOperation("列表展示")
     @GetMapping("/list")
     public List<UserInfo> list(){
         return userInfoService.list();
+    }
+
+    @PostMapping("/page")
+    public Result<?> findPage(@RequestBody PageInfo pageInfo){
+        Page<UserInfo> page = userInfoService.page(
+                new Page<>(
+                        pageInfo.getPageNum(),
+                        pageInfo.getPageSize()
+                )
+        );
+        return Result.success(page);
+    }
+
+    @PostMapping("/save")
+    public Result<?> save(@Validated @RequestBody UserInfo userInfo){
+        userInfoService.saveOrUpdate(userInfo);
+        return Result.success();
+    }
+
+    @PostMapping("/delBatch")
+    public Result<?> delBatch(@RequestBody List<Integer> ids){
+        userInfoService.removeByIds(ids);
+        return Result.success();
+    }
+
+    @GetMapping("/getlogin")
+    public Result<?> getLogin(@RequestParam("username") String username,@RequestParam("password") String password){
+        LambdaQueryWrapper<UserInfo> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(UserInfo::getUsername,username)
+                .eq(UserInfo::getPassword,password)
+                .last("limit 1");
+        UserInfo userone = userInfoService.getOne(wrapper);
+
+        if(userone!=null){
+            //生成jwt
+            String token = JwtUtils.generateToken(userone);
+            HashMap<Object, Object> map = new HashMap<>();
+            map.put("token",token);
+            map.put("username",userone.getUsername());
+            map.put("email",userone.getEmail());
+            map.put("create_time",userone.getCreateTime());
+            return Result.success(map);
+
+        }else {
+            throw new CustomException("请检查用户名密码是否正确");
+        }
+
+    }
+    @PostMapping("/postlogin")
+    public Result<?> postLogin(@Validated @RequestBody UserLoginDto userLoginDto){
+        LambdaQueryWrapper<UserInfo> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(UserInfo::getUsername,userLoginDto.getUsername())
+                .eq(UserInfo::getPassword,userLoginDto.getPassword())
+                .last("limit 1");
+        UserInfo userone = userInfoService.getOne(wrapper);
+
+        if(userone!=null){
+            //生成jwt
+            String token = JwtUtils.generateToken(userone);
+            HashMap<Object, Object> map = new HashMap<>();
+            map.put("token",token);
+            map.put("username",userone.getUsername());
+            map.put("email",userone.getEmail());
+            map.put("create_time",userone.getCreateTime());
+            return Result.success(map);
+
+        }else {
+            throw new CustomException("请检查用户名密码是否正确");
+        }
     }
 }
---
+++ src/main/java/com/agentp/cepp/dto/UserLoginDto.java
@@ -0,0 +1,25 @@
+package com.agentp.cepp.dto;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class UserLoginDto {
+    @NotBlank(message = "用户名不能为空")
+    @Length(min = 1,max = 20,message = "用户名在5-20位之间")
+    @ApiModelProperty("用户名")
+    private String username;
+
+    @NotBlank(message = "密码不能为空")
+    @Length(min = 8,max = 100,message = "密码在8-20位之间")
+    @ApiModelProperty("密码")
+    private String password;
+
+    @NotBlank(message = "邮箱不能为空")
+    @ApiModelProperty("邮箱")
+    private String email;
+}
