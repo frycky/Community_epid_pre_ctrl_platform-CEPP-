0b92d9154532e36bea4a69d94fc17421011add2e
Agentp <2569787219@qq.com>
2023-04-07 18:10:49 +0800

    aop

---
+++ src/main/java/com/agentp/cepp/configs/SpringMvcConfig.java
@@ -0,0 +1,35 @@
+package com.agentp.cepp.configs;
+
+import com.agentp.cepp.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                //允许直接访问的接口
+                .excludePathPatterns(
+//                        "/user/login",
+//                        "/article/page",
+//                        "/article/detail",
+//
+//                        "/swagger-ui/**",
+//                        "/swagger-ui.html",
+//                        "/swagger-resources/**",
+//                        "/images/**",
+//                        "/webjars/**",
+//                        "/v2/api-docs",
+//                        "/configuration/ui",
+//                        "/configuration/security"
+                );
+    }
+
+}
